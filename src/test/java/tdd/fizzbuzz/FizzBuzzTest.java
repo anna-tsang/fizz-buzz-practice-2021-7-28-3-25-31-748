8a14c9f9d9a30b21970d92ef0d5ee028e1a51818
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-02 14:00:02 +0800

    [fizz-buzz][Anna] feat: normal case test case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,19 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_order_number_1_with_normal_case(){
+        //given
+        int order = 1;
+        //when
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String result = fizzbuzz.countOff(order);
+        //then
+        assertEquals("1",result);
+    }
+
 }
